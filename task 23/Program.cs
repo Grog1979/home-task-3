489ef0d1b8b6153f4ae936ed874a615fe1656ae3
Kostya <kosg@mail.ru>
2023-03-15 01:08:26 +0300

    changed task 23

--- task 23/Program.cs
+++ task 23/Program.cs
@@ -5,7 +5,7 @@ if (n > 0)
   for (int i = 1; i <= n; i++)
     Console.Write($" {Math.Pow(i, 3)}");
 else if (n < 0)
-  for (int i = -1; i >= n; i = i-1)
+  for (int i = -1; i >= n; i = --i)
     Console.Write($" {Math.Pow(i, 3)}");
 else
   Console.Write("0");
