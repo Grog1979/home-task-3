231adff4439c0d88d937f5d0ed8d08235d70842c
Kostya <kosg@mail.ru>
2023-03-15 00:28:27 +0300

    add task 19

---
+++ task 19/Program.cs
@@ -0,0 +1,9 @@
+﻿// задача 19
+Console.Clear();
+Console.Write("Введите пятизначное число:");
+int n = Convert.ToInt32(Console.ReadLine());
+
+if (n/10000 == n%10 && n/1000 - (n/10000)*10 == (n%100 - n%10)/10)
+  Console.WriteLine("палиндром");
+else
+  Console.WriteLine("не палиндром");
